0b071aca752363ce5c5dbc7fbd2791b4c784ff41
root <qyqbird@gmail.com>
2017-02-19 16:40:31 +0800

    aaa

--- stock_select/foundmental.py
+++ stock_select/foundmental.py
@@ -7,6 +7,7 @@ import math
 import copy
 import sys
 from pandas import DataFrame 
+import pandas as pd
 sys.path.append('../utility')
 from download import DownLoad,TS
 
@@ -80,6 +81,18 @@ class StockSelect(object):
             if stock_data is not None:
                 self.delta_compute(stock_data, name, start)
 
+    def list_stock_info(self, stock_list):
+        keys = stock_list.keys()
+        data = DataFrame(self.foundmental_data, index=keys)
+        data = data.sort(columns='earn_ratio', ascending=False)   
+        for code in data.index:
+            gross_profit_rate = None   #默认值
+            if code in self.profit_data.index:
+                gross_profit_rate = self.profit_data.ix[code]['gross_profit_rate']
+            #2. 过滤毛利率过低的股票
+            if gross_profit_rate and gross_profit_rate < 15:
+                continue
+            self.print_stock_info(code, data.ix[code],gross_profit_rate)
 
     #资产净收益率,选择大蓝筹股
     def capital_earn_ratio(self):
@@ -112,8 +125,9 @@ class StockSelect(object):
         #排序
         #raw_data = raw_data.sort(columns='reservedPerShare',ascending=False)
         print '当前排序1. 资本公积金大 2. 股本小 ，策略待思考 3. 去掉了创业板'
-        raw_data = raw_data.sort_index(by=['totals'])
-        raw_data = raw_data.sort_index(by=['reservedPerShare'], ascending=False)
+        #raw_data = raw_data.sort(columns='totals')
+        raw_data = raw_data.sort_values(by=['totals'])
+        raw_data = raw_data.sort_values(by=['reservedPerShare'],ascending=False)
         for code in raw_data.index:
             if code.startswith('3'):
                 continue
@@ -123,43 +137,89 @@ class StockSelect(object):
             self.print_stock_info(code,raw_data.ix[code],gross_profit_rate)
     #打印股票详细信息
     @staticmethod
-    def print_stock_info(code,row_info, gross_profit_rate):
+    def print_stock_info(code,row_info, gross_profit_rate=None, ban_info=None):
         '''
         code 是股票代码
         gross_profit_rate 是毛利率
         虽然炒概念，也要炒概念中股票比较优质的资产
         '''
-        print "stock:{0} name:{1} 公积金:{2} 股本:{3:.2f}亿 PE:{4} ROE:{5:.3f} 毛利率:{6} 行业:{7}".format(code,
+        print "stock:{0} name:{1} 公积金:{2} 股本:{3:.2f}亿 PE:{4} ROE:{5:.3f} 毛利率:{6} 行业:{7} 解禁:{8}".format(code,
                 row_info['name'],row_info['reservedPerShare'],row_info['totals']/10000, 
-                row_info['pe'],row_info['earn_ratio'],gross_profit_rate,row_info['industry'])   
+                row_info['pe'],row_info['earn_ratio'],gross_profit_rate,row_info['industry'], ban_info)   
+
+    @staticmethod
+    def print_stock_info2(code,row_info, gross_profit_rate=None, ban_info=None,transfer=None):
+        '''
+        code 是股票代码
+        gross_profit_rate 是毛利率
+        虽然炒概念，也要炒概念中股票比较优质的资产
+        '''
+        print "{0}:{1} 公积金:{2} 股本:{3:.2f}亿 PE:{4} ROE:{5:.3f} 毛利率:{6} 解禁:{7} {8} 分红{9} 转股{10}".format(code,
+                row_info['name'],row_info['reservedPerShare'],row_info['totals']/10000, 
+                row_info['pe'],row_info['earn_ratio'],gross_profit_rate, ban_info, transfer['report_date'],
+                transfer['divi'], transfer['shares']) 
     #资本公积金，股本选股，用于高转送行情
-    def capital_reserve(self,rePerShare=3, total_share=80000, eps=0.5):
+    def capital_reserve(self,rePerShare=1, total_share=100000, eps=0.1):
         '''
         '''
         result = self.foundmental_data
-        # 1. 保留公积金 >2 元
+        ban_data = self.lift_ban_stock()
+        history = self.get_history_high_transfer()
+        # 1. 保留公积金 
         result = result[result['reservedPerShare'] > rePerShare]
-        # 2. 总股本 < 8亿
+        # 2. 总股本 
         result = result[result['totals'] <=total_share]
         # 3. 每股收益 >0
         result = result[result['esp'] > eps]
         #按照公积金排序
         result = result.sort(columns='reservedPerShare',ascending=False)
-        for code in result.index[:100]:
-            if not code.startswith('3'):
-                gross_profit_rate = None
-                if code in self.profit_data.index:
-                    gross_profit_rate = self.profit_data.ix[code]['gross_profit_rate']
-                self.print_stock_info(code, result.ix[code],gross_profit_rate)
+        for code in result.index[:200]:
+            gross_profit_rate = None
+            ban_info = None
+            transfer = None
+            if code in self.profit_data.index:
+                gross_profit_rate = self.profit_data.ix[code]['gross_profit_rate']
+            if code in ban_data.index:
+                ban_info = ban_data.ix[code]['ratio']
+            if code in history.index:
+                pass
+                # transfer = history.ix[code]
+                # self.print_stock_info2(code, result.ix[code],gross_profit_rate,ban_info,transfer)
+            else:
+                self.print_stock_info(code, result.ix[code],gross_profit_rate,ban_info)
 
+    def lift_ban_stock(self, interval=6):
+        '''
+            取即将解禁的股票，默认取半年的
+        '''
+        raw_data = None
+        start_month = int(datetime.strftime(datetime.now(), '%m'))
+        start_year = int(datetime.strftime(datetime.now(), '%Y'))
+        for month in xrange(start_month, start_month+interval):
+            foot = month / 13
+            month = month%12
+            if month == 0:
+                month = 12
+            print 'get {0} {1} 解禁数据'.format(start_year+foot, month)
+            data = ts.xsg_data(year=(start_year+foot),month=month)
+            raw_data = pd.concat([raw_data, data], axis=0)
+        raw_data.index = raw_data['code']
+        return raw_data
 
+    def get_history_high_transfer(self):
+        df = ts.profit_data(top=1500)
+        df.index = df['code']
+        return df
 if __name__ == '__main__':
     # raw_data = ts.get_hist_data('sz',start='2015-01-05',end='2015-01-09')
     # print raw_data
     select = StockSelect()
-    #elect.concept_analysis(u'充电桩')
+    #select.concept_analysis(u'充电桩')
+    #select.concept_analysis(u'黄金概念')
     #select.capital_reserve()
-    #select.capital_earn_ratio()
+    select.capital_earn_ratio()
     #select.market_index_percent('2015-12-01')
-    data = TS.load_self_select_stock('self_select_stock.txt')
-    select.list_stock_percent(data, '2015-12-01')
+    #data = TS.load_self_select_stock('self_select_stock.txt')
+    #data = TS.load_self_select_stock('lanchou.txt')
+    #select.list_stock_percent(data, '2015-12-01')
+    #select.list_stock_info(data)
