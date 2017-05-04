31496ae15b8f21e93b1b3ed9e06b6a92161f0b56
qinyuqing02 <qinyuqing02@meituan.com>
2017-05-04 21:06:48 +0800

    add 跌幅限制

--- service/shrinkage.py
+++ service/shrinkage.py
@@ -13,6 +13,9 @@ warnings.filterwarnings("ignore")
 
 
 def judge_shrinkage(data, threshold=0.02):
+    '''
+    判断一致股票最近是不是持续缩振幅
+    '''
     try:
         recentdata = data[-4:]
         recentdata.index = [1,2,3,4]
@@ -37,20 +40,48 @@ def judge_shrinkage(data, threshold=0.02):
     except Exception,e:
         return -1, 0
 
+def judge_oversold(data):
+    '''
+    判断该股票最近跌幅是多少，是否抄底
+    '''
+    try:
+        jieduan = data['close'][-36:]
+        high = np.max(jieduan)
+        close = np.array(jieduan)[-1]
+        ratio = (close - high) / high
+        return ratio
+    except Exception,e:
+        return 0
+
+
 class Shrinkage(object):
     def __init__(self):
         pass
 
     def process(self):
         fo = open('shrink_code', 'w')
+        oversold = open('超跌', 'w')
         raw_data = TS.memchaced_data(ts.get_stock_basics,'get_stock_basics')
+        raw_data['earn_ratio'] = raw_data['esp'] / raw_data['bvps']
         for code in raw_data.index:
             try:
+                ratio = judge_oversold(daydata)
+                if ratio < -0.45:
+                    oversold.write("{0}\t{1}\n".format(code, ratio))
+                if ratio > -0.3:
+                    continue
+                if raw_data.ix[code]['earn_ratio'] < 0.05:
+                    continue
                 totals = raw_data.ix[code]['totals']
                 daydata = ts.get_k_data(code, ktype='D')
-                if np.array(daydata['close'])[-1] * totals > 320:
+                close = np.array(daydata['close'])[-1]
+                if close < 6:
+                    continue
+                if close * totals > 320:
                     continue
                 flag,mean = judge_shrinkage(daydata,0.012)
+                if ratio > -0.3:
+                    continue
                 if flag != -1:
                     fo.write("{0}\t{1}\t{2:.1%}\n".format(flag,code, mean))
             except Exception, e:
