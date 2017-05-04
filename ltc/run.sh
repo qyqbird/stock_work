032ed47602f1f4776ddec7d89c85e2507bdc2db2
root <qyqbird@gmail.com>
2017-05-04 19:24:03 +0800

    del some

--- ltc/run.sh
+++ ltc/run.sh
@@ -1,6 +1,6 @@
 #启动代理
 python /root/down_load/stratum-mining-proxy/mining_proxy.py -pa scrypt -o coinotron.com -p 3334
-./minerd -a scrypt -x http:://127.0.0.1:3334 -u buring -p xiao11lang 
+./minerd -a scrypt -x http:://127.0.0.1:3334 -u username -p password
 
 #不启用代理
 #./minerd -a scrypt -o stratum+tcp://coinotron.com:3334 --userpass=buring:xiao11lang 
